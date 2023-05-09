ff6070cd7d04dfe93f4e1a5ed5cd808b4d8254ad
Oleksandr Sheremet <88sheremet@gmail.com>
2023-05-09 11:18:51 +0300

    create vue project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="app">
+    <post-form  @create="createPost" />
+    <post-list :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+
+export default {
+  components: {
+    PostList,
+    PostForm,
+  },
+
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "JavaScript", body: "Description" },
+        { id: 2, title: "JavaScript 2", body: "Description 2" },
+        { id: 3, title: "JavaScript 3", body: "Description 3" },
+      ],
+    };
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
